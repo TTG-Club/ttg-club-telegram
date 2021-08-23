78c2b693791f9938cc7379cc4a34e9086e06efb4
svifty7 <svifty7@gmail.com>
2021-08-23 12:55:54 +0300

    get spell info from dungeon.su

--- src/types/bot.d.ts
+++ src/types/bot.d.ts
@@ -1,11 +1,11 @@
 import { Context, Scenes } from 'telegraf';
-import IDB from './db';
+import * as HTMLParser from 'node-html-parser';
 
 declare namespace IBot {
     interface ISceneSession extends Scenes.SceneSessionData {
         sceneSessionProp: number
         spellName: string
-        previousSpells: IDB.ISpell[]
+        previousSpells: IBot.ISpell[]
     }
 
     interface ISession extends Scenes.SceneSession<IBot.ISceneSession> {
@@ -26,6 +26,12 @@ declare namespace IBot {
 
     interface ICommands {
         [key: string]: ICommand
+    }
+
+    interface ISpell {
+        name: string
+        url: string,
+        el: HTMLParser.HTMLElement
     }
 }
 
