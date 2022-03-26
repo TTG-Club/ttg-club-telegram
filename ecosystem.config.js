408277febf717604d2dab0d5fb152ed7b2a1253c
svifty7 <svifty7@gmail.com>
2022-03-27 00:19:06 +0300

    update ci/cd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
     apps: [{
         name: 'dnd-tg-bot',
         script: 'dist/telegram/TelegramBot.js',
-        watch: 'dist/telegram'
+        watch: 'dist/telegram',
+        exp_backoff_restart_delay: 100
     }]
 };
