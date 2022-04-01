c5a64a6b6e4eaddec69318401969b3d30a7ea9bf
svifty7 <svifty7@gmail.com>
2022-04-01 15:01:02 +0300

    slice inline results

--- src/telegram/actions/SpellActions.ts
+++ src/telegram/actions/SpellActions.ts
@@ -45,47 +45,49 @@ bot.inlineQuery(/.*/, async ctx => {
         }]
     };
     const result: NSpell.ISpell[] = await http.post('/spells', apiOptions);
-    const spells: InlineQueryResult[] = result.map((spell, index) => {
-        const level = spellsMiddleware.getLevel(spell.level);
-        const school = spellsMiddleware.getSchool(spell.school);
-        const source = spellsMiddleware.getSource(spell.source);
-        const ritual = spell?.meta?.ritual
-            ? '(ритуал)'
-            : '';
+    const spells: InlineQueryResult[] = result
+        .map((spell, index) => {
+            const level = spellsMiddleware.getLevel(spell.level);
+            const school = spellsMiddleware.getSchool(spell.school);
+            const source = spellsMiddleware.getSource(spell.source);
+            const ritual = spell?.meta?.ritual
+                ? '(ритуал)'
+                : '';
 
-        let msg = spellsMiddleware.getSpellMessage(spell).messages.join();
+            let msg = spellsMiddleware.getSpellMessage(spell).messages.join();
 
-        const isBig = msg.length > 3750;
+            const isBig = msg.length > 3750;
 
-        if (isBig) {
-            const add = '...\n\n<b>Заклинание показано без форматирования, чтобы постараться уместить его в одно'
+            if (isBig) {
+                const add = '...\n\n<b>Заклинание показано без форматирования, чтобы постараться уместить его в одно'
                 + ' сообщение и может быть обрезано, пожалуйста, посмотрите оригинал на сайте по кнопке ниже или'
                 + ' напишите боту в личные сообщения</b> 😉';
 
-            msg = spellsMiddleware.getSpellMessage(spell, true).messages.join();
-            msg = msg.slice(0, 3750).trim() + add;
-        }
+                msg = spellsMiddleware.getSpellMessage(spell, true).messages.join();
+                msg = msg.slice(0, 3750).trim() + add;
+            }
 
-        return {
-            type: 'article',
-            id: String(index),
-            title: `${ spell.name } [${ spell.englishName }]`,
-            url: spellsMiddleware.getOriginal(spell.englishName),
-            hide_url: false,
-            description: `${ level }, ${ school } ${ ritual }`
+            return {
+                type: 'article',
+                id: String(index),
+                title: `${ spell.name } [${ spell.englishName }]`,
+                url: spellsMiddleware.getOriginal(spell.englishName),
+                hide_url: false,
+                description: `${ level }, ${ school } ${ ritual }`
                 + `\n${ source }`,
-            thumb_url: `${ config.baseURL }/resources/assets/icon/avatar.png`,
-            input_message_content: {
-                message_text: msg,
-                parse_mode: 'HTML',
-                disable_web_page_preview: true,
-            },
-            reply_markup: Markup.inlineKeyboard([
-                [ Markup.urlButton('Оригинал на D&D5 Club', spellsMiddleware.getOriginal(spell.englishName)) ],
-                [ Markup.urlButton('Перейти к боту', `https://t.me/${ ctx.botInfo?.username }`) ]
-            ]),
-        }
-    });
+                thumb_url: `${ config.baseURL }/resources/assets/icon/avatar.png`,
+                input_message_content: {
+                    message_text: msg,
+                    parse_mode: 'HTML',
+                    disable_web_page_preview: true,
+                },
+                reply_markup: Markup.inlineKeyboard([
+                    [ Markup.urlButton('Оригинал на D&D5 Club', spellsMiddleware.getOriginal(spell.englishName)) ],
+                    [ Markup.urlButton('Перейти к боту', `https://t.me/${ ctx.botInfo?.username }`) ]
+                ]),
+            }
+        })
+        .slice(0, 49) as InlineQueryResult[];
 
     await ctx.answerInlineQuery(spells, {
         switch_pm_text: 'Перейти в бота',
