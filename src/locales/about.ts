fccc3d296c02855166862efa632b5e8eb0978a0f
svifty7 <svifty7@gmail.com>
2022-10-25 22:45:42 +0300

    dnd5 -> ttg

--- src/locales/about.ts
+++ src/locales/about.ts
@@ -1,11 +1,11 @@
 export const ABOUT_MESSAGE = 'Этот бот служит дополнением для онлайн-справочника DnD5 Club, '
-    + 'доступного по этой ссылке: https://dnd5.club/'
+    + 'доступного по этой ссылке: https://ttg.club/'
     + '\n\nМы ждем тебя в наших социальных сетях, присоединиться можно по кнопкам ниже ☺️';
 
 export const SOCIAL_LINKS = {
     site: {
         label: 'Сайт DnD5 Club',
-        url: 'https://dnd5.club/'
+        url: 'https://ttg.club/'
     },
     discord: {
         label: 'Discord-канал',
