2b41cd3b92a2d312d7a02e29d7b3e023544a84a2
svifty7 <svifty7@gmail.com>
2024-01-22 10:19:16 +0300

    fix: remove keyboard

--- src/commands/base/remove-keyboard.ts
+++ src/commands/base/remove-keyboard.ts
@@ -5,13 +5,12 @@ const COMMAND_NAME = 'remove_keyboard';
 const removeKeyboardCommand: ICommand = {
   command: COMMAND_NAME,
   description: 'Удалить клавиатуру',
-  fullDescription: `/${COMMAND_NAME} - удаление фантомной клавиатуры`,
+  fullDescription: `/${COMMAND_NAME} - удаление клавиатуры в чате`,
   callback: async ctx => {
     try {
       const reply = await ctx.reply('Сейчас все почистим...', {
         disable_notification: true,
         reply_markup: {
-          selective: true,
           input_field_placeholder: '',
           keyboard: []
         }
